2a0db73a2bdcacb32d7402c7966ceca544c7556f
Rob Kutner <Rob.Kutner@gmail.com>
2019-09-10 21:37:44 -0700

    worked on structure of quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,57 @@
-var questions= [
-    ["Who is the one true god?\n A: Azathoth\n B: Torm\n C: Mir Yannik\n D. What the hell there's no way I'm dealing with that conversation in class. Next question."], ["d"]
-    ["Who is the best cricketer of all time?\n A: Virat Kohli\n B:Sachin Tendulkar\n C: Don Bradman D: What the hell is a cricketer?"],["d"]
-    ["Feather Bowling comes from which country?\n A: China\n B: Chad\n C: Belgium\n D: FEATHER bowling? You just made that up"]["d"]
-    ["Who really controls the US government?\n A: The Inebriati\n B: The Knights Tipplar\n C: Yo mama\n D: Okay, are ANY of these going to be real questions?"]["d"]
-    ["What does the fox say?\n A: Gering-ding-ding-ding-dingeringeding!\n B: Wa-pa-pa-pa-pa-pa-pow!\n C:Hatee-hatee-hatee-ho!\n D: If you can't take this seriously, I'm leaving"]["d"]
-    ["Bah-weep-Graaaaagnah wheep ni ni bong?\n A: Look...\n B:...\n C:...\n D: I hate you."][d]
-]
-
 window.onload = function(){
     var clockRunning = false;
-    var score;
-    $("timer").text = 60;
+    var clockRunning;
+    var score=0;
+    var timer=60;
+    $("#timer").text = timer;
 
-}
+var questions= [
+    ["Who is the one true god?", "Azathoth", "The Flying Spaghetti Monster", "Bilious", "What the hell there's no way I'm dealing with that conversation in class. Next question."],
+    ["Who is the best cricketer of all time?", "Virat Kohli", "Sachin Tendulkar", "Don Bradman", "What the hell is a cricketer?"],
+    ["Feather Bowling comes from which country?", "China", "Chad", "Belgium", "FEATHER bowling? You just made that up"],
+    ["Who really controls the US government?", "The Inebriati", "The Knights Tipplar", "Yo mama", "Okay, are ANY of these going to be real questions?"],
+    ["What does the fox say?", "Gering-ding-ding-ding-dingeringeding!", "Wa-pa-pa-pa-pa-pa-pow!", "Hatee-hatee-hatee-ho!", "If you won't take this seriously, I'm leaving"],
+    ["Bah-weep-Graaaaagnah wheep ni ni bong?", "Look...", "...", "...", "I hate you."]
+]
+$("#startTimer").on('click', (event)=> {
+    event.preventDefault();
+    start()
+    quiz(questions)
+});
+
+
 
 function start(){
     if (!clockRunning){
         intervalId= setInterval(tick, 1000);
         clockRunning=true;
+    }
+}
+
+function tick(){
+    timer--
+    $("#timer").text = timer;
+}
+
+
+
+function quiz(arr){
+    for (let i=0; i<4; i++){
+        $(".question")= $("<h1>")
+        $(".answerA")= $("<h3>")
+        $(".answerA").attr('type', 'radio')
+        $(".answerA").text(arr[i])
+        $(".answerB")= $("<h3>")
+        $(".answerB").attr('type', 'radio')
+        $(".answerB").text(arr[i])
+        $(".answerC")= $("<h3>")
+        $(".answerC").attr('type', 'radio')
+        $(".answerC").text(arr[i])
+        $(".answerD")= $("<h3>")
+        $(".answerD").attr('type', 'radio')
+        $(".answerD").text(arr[i])
+
+    }
+
     }
 }
